d121e931f14aa618fd5d244f610bb2266b6c1ae3
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-14 20:11:12 -0500

    Scraping a live website

--- main.py
+++ main.py
@@ -1,37 +1,72 @@
 from bs4 import BeautifulSoup
-with open('website.html', encoding="utf8") as file:
-    contents = file.read()
+import requests
 
-soup = BeautifulSoup(contents, 'html.parser')
-print(soup.title)
-print(soup.title.string)
+response = requests.get("https://news.ycombinator.com/")
 
-print(soup.prettify())
+yc_web_page = response.text
 
-print(soup.a)
+soup = BeautifulSoup(yc_web_page, 'html.parser')
 
-all_anchor_tags = soup.find_all(name="a")
+# article_text = soup.select_one(selector=".titleline a" ).getText()
 
-anchor_tags_texts = [tag.getText() for tag in all_anchor_tags]
-print(anchor_tags_texts)
+article_text = soup.select_one(selector=".titleline a" ).getText()
 
-anchor_tags_links = [tag.get("href") for tag in all_anchor_tags]
-print(anchor_tags_links)
+print(article_text)
+articles = soup.find_all(name="span", class_="titleline")
 
-heading = soup.find(name="h1", id="name")
+articles_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+print(f"votes: {articles_upvotes}")
 
-print(heading)
+articles_dic = [{f"{article.find(name='a').getText()}": article.find(name='a').get("href")} for article in articles]
+print(articles_dic)
 
-section_heading = soup.find(name="h3", class_="heading")
-print(section_heading)
-print(section_heading.name)
-print(section_heading.get("class"))
+largest_number = max(articles_upvotes)
+print(largest_number)
 
-company_url = soup.select_one(selector="p a").get("href")
-print(company_url)
+index_of_largest_number = articles_upvotes.index(largest_number)
+print(index_of_largest_number)
 
-name = soup.select_one(selector="#name")
-print(name)
+article_of_largest_number = articles_dic[index_of_largest_number]
+print(article_of_largest_number)
 
-headings = soup.select(".heading")
-print(headings)
+
+
+
+
+
+# with open('website.html', encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+# print(soup.title)
+# print(soup.title.string)
+#
+# print(soup.prettify())
+#
+# print(soup.a)
+#
+# all_anchor_tags = soup.find_all(name="a")
+#
+# anchor_tags_texts = [tag.getText() for tag in all_anchor_tags]
+# print(anchor_tags_texts)
+#
+# anchor_tags_links = [tag.get("href") for tag in all_anchor_tags]
+# print(anchor_tags_links)
+#
+# heading = soup.find(name="h1", id="name")
+#
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+# print(section_heading.name)
+# print(section_heading.get("class"))
+#
+# company_url = soup.select_one(selector="p a").get("href")
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+#
+# headings = soup.select(".heading")
+# print(headings)
